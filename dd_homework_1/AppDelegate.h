7a569ddef0a380a75284d0b6f2895494b1ad0f4d
Andrey <andrersv96@gmail.com>
2017-10-08 00:50:09 +0300

    Initial Commit

---
+++ dd_homework_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dd_homework_1
+//
+//  Created by Andrey on 08/10/2017.
+//  Copyright © 2017 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dd_homework_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dd_homework_1
+//
+//  Created by Andrey on 08/10/2017.
+//  Copyright © 2017 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
